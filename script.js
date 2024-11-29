889281639abcc53b86d427adb07f6c95d0a30dd8
Tanya Parmar <tanyaparmar03@gmail.com>
2024-11-29 22:29:47 +0530

    multiply divide operation in calculator

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function addNumbers() {
     const num2 = parseFloat(document.getElementById('num2').value);
     document.getElementById('result').innerText = num1 - num2;
 }
+
+function multiplyNumbers(){
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    document.getElementById('result').innerText = num1 * num2;
+}
+
+function divideNumbers(){
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    if(num2 != 0){
+        document.getElementById('result').innerText = num1 / num2;
+    }else{
+        document.getElementById('result').innerText = "divide by zero exception";
+    }
+    
+}
