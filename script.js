0d8f1529317575f3d146f6023c26ea7d9a12b816
Tanya Parmar <tanyaparmar03@gmail.com>
2024-11-29 22:25:46 +0530

    sub operation in calculator

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function addNumbers() {
     const num2 = parseFloat(document.getElementById('num2').value);
     document.getElementById('result').innerText = num1 + num2;
   }
+
+  function subtractNumbers() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    document.getElementById('result').innerText = num1 - num2;
+}
