955aa43d4aa4d6f294d2f9a777a7ca5820f511bc
Tanya Parmar <tanyaparmar03@gmail.com>
2024-11-29 22:23:52 +0530

    add operation in calculator

---
+++ script.js
@@ -0,0 +1,5 @@
+function addNumbers() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    document.getElementById('result').innerText = num1 + num2;
+  }
